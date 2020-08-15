ce23e3cad213f15d7aff883c6bf3784da19b93ff
Xiangdi Chai <sc17xc@leeds.ac.uk>
2020-08-15 20:08:34 +0800

    vessel class implemented

--- Vessel.java
+++ Vessel.java
@@ -1,5 +1,23 @@
 
 
 public class Vessel{
+    protected int capacity;
+
+
+    public Vessel() {
+    }
+
+    public Vessel(long id, int capacity) {
+        super(id);
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 
 }
